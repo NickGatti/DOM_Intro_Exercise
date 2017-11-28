b405f07831846377ec8dff104d1cf42efcb54171
NickGatti <nick.gatti@gmail.com>
2017-11-28 14:55:05 -0700

    which one is selected is selected and others arent

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ let listItems = document.querySelectorAll( 'li' )
 
 for ( let i = 0; i < listItems.length; i++ ) {
     listItems[ i ].style.backgroundColor = 'yellow'
+    listItems[ i ].addEventListener( 'click', ( ( event ) => {
+        listItems[ i ].className = 'selected'
+        for ( let z = 0; z < listItems.length; z++ ) {
+            if ( z !== i ) listItems[ z ].className = ''
+        }
+    } ) )
 }
