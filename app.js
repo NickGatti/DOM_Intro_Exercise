4515c8034a7658debaa5e027b567c01eb0beff3c
NickGatti <nick.gatti@gmail.com>
2017-11-28 14:49:54 -0700

    init

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Javascript is alive!");
+console.log( "Javascript is alive!" );
+
+greeting = document.querySelector( '#greeting' )
+
+greeting.innerHTML = 'Hello, World!'
+
+let newImg = document.createElement( 'img' )
+newImg.src = 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif'
+
+greeting.appendChild( newImg )
+
+let listItems = document.querySelectorAll( 'li' )
+
+for ( let i = 0; i < listItems.length; i++ ) {
+    listItems[ i ].style.backgroundColor = 'yellow'
+}
